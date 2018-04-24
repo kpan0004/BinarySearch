510eb4642af6c837e93e4dc39d33eca1df4c85b1
kpan0004 <37351061+kpan0004@users.noreply.github.com>
2018-04-24 14:26:29 +1000

    Edited with documentation

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,12 @@
 def binary_search(a_list, a_value):
+    """
+    Performs binary search on a list that is sorted from min to max
+    : Input: a_list, the sorted list to be searched
+    : Input: a_value, the value for which we are searching
+    : Returns: True if a_value is in the list, False otherwise
+    : Returns: num_iterations, the number of iterations required to perform the
+    search
+    """
     lower = 0
     upper = len(a_list) - 1
     num_iterations = 1
@@ -14,6 +22,14 @@ def binary_search(a_list, a_value):
     return False, num_iterations
 
 def binary_search_recursive_aux(a_list, a_value, lower, upper):
+    """
+    Auxiliary function that performs binary search with recursion on a list that is sorted from min to max
+    : Input: a_list, the sorted list to be searched
+    : Input: a_value, the value for which we are searching
+    : Input: lower, lower index of the search
+    : Input: upper, upper index of the search
+    : Returns: True if a_value is in the list, False otherwise
+    """
     mid = (lower + upper)//2
     if lower > upper:
         return False
@@ -24,6 +40,13 @@ def binary_search_recursive_aux(a_list, a_value, lower, upper):
     return binary_search_recursive_aux(a_list, a_value, lower, mid - 1)
 
 def binary_search_recursive(a_list, a_value):
+    """
+    Function that performs binary search with recursion on a list that is sorted from min to max. Uses the auxiliary function to set the initial lower and
+    upper to be the first and last indicies (0 and len(a_list) - 1) respectively
+    : Input: a_list, the sorted list to be searched
+    : Input: a_value, the value for which we are searching
+    : Returns: True if a_value is in the list, False otherwise
+    """
     return binary_search_recursive_aux(a_list, a_value, 0, len(a_list) -1)
         
 
@@ -31,11 +54,4 @@ if __name__ == "__main__":
     my_list = [i for i in range(1,101, 2)]
     print(binary_search_recursive(my_list, 74))
 
-    """
-    Performs binary search on a list that is sorted from min to max
-    : Input: a_list, the sorted list to be searched
-    : Input: a_value, the value for which we are searching
-    : Returns: True if a_value is in the list, False otherwise
-    : Returns: num_iterations, the number of iterations required to perform the
-    search
-    """
+
